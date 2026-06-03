a4e614ec6e2f777d29ad542cedc7a87968d2fcb7
Edwin guevara <egbmaster2007@gmail.com>
2026-06-03 10:14:50 -0500

    fix: explicitly define db driver class

--- frontend/src/app/(blank-layout-pages)/account-setup/page.tsx
+++ frontend/src/app/(blank-layout-pages)/account-setup/page.tsx
@@ -5,7 +5,7 @@ import AccountSetup from '@/views/pages/auth/AccountSetup'
 const AccountSetupPage = () => {
   return (
     <div className='flex flex-col justify-center items-center min-bs-[80dvh] p-6'>
-      <AccountSetup />
+      <AccountSetup /> 
     </div>
   )
 }
